0c79c7109879cf845be809497aa94355932a2337
Josef Grosch <jgrosch@gmail.com>
2024-02-16 21:25:52 -0800

    Laying out the Web interface

---
+++ gdead/src/main/java/com/mooseriver/gdead/Common.java
@@ -0,0 +1,14 @@
+package com.mooseriver.gdead;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jgrosch
+ */
+public class Common {
+    
+}
